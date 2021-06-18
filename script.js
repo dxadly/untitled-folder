eea65b9db1f1675c8f8586734c6f7c8998c57cce
dxadly <adlydavid2@gmail.com>
2021-06-17 18:30:56 -0700

    f

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+import * as THREE from "https://cdn.skypack.dev/three";
+
+import { GLTFLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/DRACOLoader.js";
+
+import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
+
+import store from "./store.js";
 
 const backgroundColor = 0x000000;
 
